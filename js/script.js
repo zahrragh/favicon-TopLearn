e87f662bbdaac83464c2a172719b1bb444dd3664
zahra <zahrraghh@gmail.com>
2021-09-25 10:26:16 +0330

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const favicon=document.getElementById('favicon')
+
+function toGreen(){
+    favicon.setAttribute('href','img/faviconTwo.png')
+}
+function toRed(){
+    favicon.setAttribute('href','img/favicon.png')
+}
